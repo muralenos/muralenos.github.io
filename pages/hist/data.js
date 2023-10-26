3972cb471eb75028143d3ea7354eccee35fb805e
Comite Muraleño de Santo Domingo de Heredia <147711677+muralenos@users.noreply.github.com>
2023-10-26 08:15:05 -0600

    Add files via upload

--- pages/hist/data.js
+++ pages/hist/data.js
@@ -4,8 +4,8 @@ dm.fn.loadDataset("hist", {
     "title": "Historico/Cultural",
     "shorttitle": "Hist./Cult.",
 
-    "ids": ["1"],
-    "show": ["1"],
+    "ids": ["1", "2", "3", "4" ],
+    "show": ["4", "1", "2", "3" ],
 
     buildDetail: function (data, cb) {
 
